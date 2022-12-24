98eb4967e47f6bf440a49448921eaa0e7619225a
ccccy666 <119689373+ccccy666@users.noreply.github.com>
2022-12-24 23:55:58 +0800

    Create error.h
    错误类

---
+++ error.h
@@ -0,0 +1,37 @@
+#ifndef CODE_ERROR_HPP
+#define CODE_ERROR_HPP
+
+#include <string>
+#include <exception>
+
+
+class ErrorException : public std::exception {
+public:
+    explicit ErrorException(std::string message);
+
+    std::string getMessage() const;
+
+private:
+    std::string message;
+};
+
+//------------------------------------------------------------------------------------------------
+
+void error(std::string message);
+
+
+ErrorException::ErrorException(std::string message) {
+    this->message = message;
+}
+
+std::string ErrorException::getMessage() const {
+    return message;
+}
+
+//----------------------------------------------------------------------------------------
+
+void error(std::string message) {
+    throw ErrorException(message);
+}
+
+#endif //CODE_ERROR_HPP
