10e046e6768351269e383d1716346bb65ee19e68
ccccy666 <119689373+ccccy666@users.noreply.github.com>
2022-12-29 23:26:58 +0800

    Update book.h
    利用模板块状链表重构了Book类

--- book.h
+++ book.h
@@ -5,12 +5,14 @@
 #include"error.h"
 //#include <cstring>
 #include<vector>
+#include"linklist_.h"
 
 using namespace std;
 struct ISBN {
     char isbn_[22];
 
     ISBN(std::string &in) {
+        if(in.size()>20)error("Invalid");
         for (int i = 0; i < in.size(); i++) {
             isbn_[i] = in[i];
         }
@@ -31,7 +33,7 @@ struct ISBN {
         return ((strcmp(isbn_, other.isbn_) < 0) || (!strcmp(isbn_, other.isbn_)));
     }
 
-    friend ostream &operator<<(ostream &out, ISBN &the) {
+    friend ostream &operator<<(ostream &out,const ISBN &the) {
         out << the.isbn_;
         return out;
     }
@@ -41,13 +43,14 @@ struct Name {
     char name_[62];
 
     Name(std::string &in) {
+        if(in.size()>60)error("Invalid");
         for (int i = 0; i < in.size(); i++) {
             name_[i] = in[i];
         }
         name_[in.size()] = '\0';
     }
 
-    Name() {};
+    Name() {name_[0]='\0';};
 
     bool operator==(const Name &other) const{
         return (!strcmp(name_, other.name_));
@@ -67,13 +70,15 @@ struct Author {
     char author_[62];
 
     Author(std::string &in) {
+        if(in.size()>60)error("Invalid");
+
         for (int i = 0; i < in.size(); i++) {
             author_[i] = in[i];
         }
         author_[in.size()] = '\0';
     }
 
-    Author() {};
+    Author() {author_[0]='\0';};
 
     bool operator==(Author &other) const{
         return (!strcmp(author_, other.author_));
@@ -93,13 +98,15 @@ struct Keyword {
     char keyword_[62];
 
     Keyword(std::string &in) {
+        if(in.size()>60)error("Invalid");
+
         for (int i = 0; i < in.size(); i++) {
             keyword_[i] = in[i];
         }
         keyword_[in.size()] = '\0';
     }
 
-    Keyword() {};
+    Keyword() {keyword_[0]='\0';};
 
     bool operator==(Keyword &other) const{
         return (!strcmp(keyword_, other.keyword_));
@@ -121,7 +128,7 @@ public:
     Name name;
     Author author;
     Keyword keyword;
-    int quantity = 0;
+    long long quantity = 0;
     double price = 0;
     double total_cost = 0;
 
@@ -149,339 +156,87 @@ public:
     ~Book() {};
 
     friend ostream &operator<<(ostream &out, Book &the) {
-        out << the.isbn << '\t' << the.name << '\t' << the.author << '\t' << the.keyword << '\t' << the.price << '\t'
-            << the.quantity << '\t';
+        int m=int(the.price);//n=int(the.total_cost);
+        string ss1= to_string(the.price);//ss2= to_string(the.quantity);
+        string s1=to_string(m);//s2= to_string(n);
+        //cout<<the.price<<' ';
+        //cout<<ss1<<' '<<s1<<' ';
+        out << the.isbn << '\t' << the.name << '\t' << the.author << '\t' << the.keyword << '\t';
+        if(ss1.size()>=s1.size()+3){
+            //out.p(s1.size()+2)
+            for(int i=0;i<s1.size()+3;i++){
+                out<<ss1[i];
+            }
+            out << '\t';
+        }else if(ss1.size()==s1.size()+2){
+            out<<ss1 <<'0'<< '\t';
+        }else if(ss1.size()==s1.size()){
+            out<<ss1 <<".00"<< '\t';
+        }
+        out<<the.quantity;
+//        if(ss2.size()>=s2.size()+3){
+//            out.precision(s2.size()+2);
+//            out<<the.total_cost << '\t';
+//        }else if(ss2.size()==s2.size()+2){
+//            out<<the.total_cost <<'0'<< '\t';
+//        }else{
+//            out<<the.total_cost <<".00"<< '\t';
+//        }
         return out;
     }
 };
 
-struct Block {
-    int next = -1;
-    Book mini, maxi;
-    int size = 0;
-    Book ele[2001];
-    int place;
-};
 
-class All_Books {
-
+class Bookstore{
 public:
-    const int maxsize = 2000, minsize = 1000;
-    int total = -1;
-    fstream inout;
-    string filename = "All_books";
-
-    All_Books() {
-//        filename=file;
-        inout.open(filename);
-        if (!inout) {
-            Block tmp;
-            ofstream out;
-            out.open(filename);
-            total = -1;
-            out.write(reinterpret_cast<char *>(&total), sizeof(total));
-            out.write(reinterpret_cast<char *>(&tmp), sizeof(tmp));
-            out.close();
-            inout.open(filename);
-            return;
-        }
-        inout.seekg(0);
-        inout.read(reinterpret_cast<char *>(&total), sizeof(total));
-    }
-
-
-    ~All_Books() {
-        inout.seekp(0);
-        inout.write(reinterpret_cast<char *>(&total), sizeof(total));
-        inout.close();
-    }
-
-    void getin(int coord, Block &node) {
-        if (coord < 0) {
-            exit(1);
-        }
-        inout.seekg(coord * sizeof(Block) + sizeof(int));
-        inout.read(reinterpret_cast<char *>(&(node.next)), sizeof(node.next));
-        inout.read(reinterpret_cast<char *>(&(node.mini)), sizeof(node.mini));
-        inout.read(reinterpret_cast<char *>(&(node.maxi)), sizeof(node.maxi));
-        inout.read(reinterpret_cast<char *>(&(node.size)), sizeof(node.size));
-    }
-
-    void myread(int coord, Block &node) {
-        if (coord < 0) {
-            exit(1);
-        }
-        inout.seekg(coord * sizeof(Block) + sizeof(int));
-        inout.read(reinterpret_cast<char *>(&node), sizeof(node));
-    }
-
-    void mywrite(int coord, Block &node) {
-        if (coord < 0) {
-            exit(1);
-        }
-        inout.seekp(coord * sizeof(Block) + sizeof(int));
-        inout.write(reinterpret_cast<char *>(&node), sizeof(node));
-    }
-
-    void split(int coord, Block &node) {
-        total++;
-        Block new_;
-        for (int i = minsize; i < node.size; ++i) {
-            new_.ele[i - minsize] = node.ele[i];
-        }
-        new_.size = minsize + 1;
-        new_.next = node.next;
-        new_.place=total;
-        node.next = total;
-        node.size = minsize;
-        node.maxi = node.ele[node.size - 1];
-        node.mini = node.ele[0];
-        new_.maxi = new_.ele[node.size];
-        new_.mini = new_.ele[0];
-        mywrite(coord, node);
-        mywrite(total, new_);
-    }
-
-    void insert_sort(int coord, const Book &ele) {
-        Block new_;
-        if (total == -1) {
-            ++total;
-            new_.place=0;
-            new_.size = 1;
-            new_.mini = new_.maxi = ele;
-            new_.ele[0] = ele;
-            mywrite(total, new_);
-            return;
-        }
-        myread(coord, new_);
-        int tmp = 0;
-        for (int i = 0; i < new_.size; ++i) {
-            if (ele < new_.ele[i]) {
-                break;
-            }
-            tmp++;
-        }
-        for (int i = new_.size; i > tmp; --i) {
-            new_.ele[i] = new_.ele[i - 1];
-        }
-        new_.ele[tmp] = ele;
-        ++new_.size;
-        if (new_.size > maxsize) {
-            split(coord, new_);
-        } else {
-            new_.mini = new_.ele[0];
-            new_.maxi = new_.ele[new_.size - 1];
-            mywrite(coord, new_);
-        }
-    }
-
-    void insert(const Book &ele) {
-        Block new_;
-        getin(0, new_);
-        if (new_.size == 0 || ele < new_.mini) {
-            insert_sort(0, ele);
-            return;
-        }
-        Block nx;
-        int i = 0;
-        while (true) {
-            if (new_.next == -1) {
-                break;
-            }
-            getin(new_.next, nx);
-            if (new_.mini <= ele && ele < nx.mini) {
-                // 插入这里
-                insert_sort(i, ele);
-                return;
-            }
-            i = new_.next;
-            new_ = nx;
-        }
-        insert_sort(i, ele);
-    }
-
-    void find(const ISBN &inde) {
-        Block new_;
-        int i = 0;
-        //bool fin = 0;
-        while (i != -1) {
-            getin(i, new_);
-            if (new_.size == 0) {
-                i = new_.next;
-                continue;
-            }
-            if ((new_.mini.isbn <= inde) && (inde <= new_.maxi.isbn)) {
-
-                myread(i, new_);
-                for (int k = 0; k < new_.size; ++k) {
-                    if (new_.ele[k].isbn == inde) {
-                        //fin = true;
-                        std::cout << new_.ele[k];
-                        cout << "\n";
-                        return;
-                    }
-                }
-            } else if (inde < new_.mini.isbn) {
-                break;
-            }
-            i = new_.next;
-        }
-
-        cout << '\n';
-        //return;
+    linklist_<ISBN,Book>list;
+    Bookstore():list(" Books"){
 
     }
-
-    void merge(int coord, Block &node) {
-        int nx1 = node.next;
-        if (nx1 == -1) {
-            mywrite(coord, node);
-            return;
-        }
-        Block nx;
-        myread(nx1, nx);
-        if (node.size + nx.size <= maxsize) {
-            for (int i = node.size; i < node.size + nx.size; ++i) {
-                node.ele[i] = nx.ele[i - node.size];
-            }
-            node.size += nx.size;
-            node.next = nx.next;
-            node.mini = node.ele[0];
-            node.maxi = node.ele[node.size - 1];
-        } else if (nx.size > minsize) {
-            node.maxi = node.ele[node.size] = nx.ele[0];
-            node.size++;
-            nx.mini = nx.ele[1];
-            for (int i = 1; i < nx.size; i++) {
-                nx.ele[i - 1] = nx.ele[i];
-            }
-            nx.size--;
-            mywrite(node.next, nx);
-        }
-        mywrite(coord, node);
-
+    ~Bookstore(){}
+    void insert (const point<ISBN,Book> &inde){
+        list.insert(inde);
     }
-
-    void delete_sort(int coord, const Book &ele) {
-        Block new_;
-        myread(coord, new_);
-        for (int i = 0; i < new_.size; ++i) {
-            if (new_.ele[i] == ele) {
-                for (int j = i + 1; j < new_.size; ++j) {
-                    new_.ele[j - 1] = new_.ele[j];
-                }
-                --new_.size;
-                if (!new_.size) {
-                    mywrite(coord, new_);
-                    break;
-                }
-                new_.mini = new_.ele[0];
-                new_.maxi = new_.ele[new_.size - 1];
-                if (new_.size < minsize) {
-                    merge(coord, new_);
-                } else {
-                    mywrite(coord, new_);
-                }
-                break;
-            }
-        }
+    block__<ISBN,Book> getblock(const ISBN &inde){
+        //cout<<inde;
+        return list.getblock(inde);
     }
-
-    void del(const Book &ele) {
-        Block new_;
-        int i = 0;
-        while (i != -1) {
-            getin(i, new_);
-            if (new_.size == 0) {
-                i = new_.next;
-                continue;
-            }
-            if (new_.mini <= ele && ele <=new_.maxi ) {
-                delete_sort(i, ele);
-                break;
-            }
-            if (ele < new_.mini) {
-                break;
-            }
-            i = new_.next;
-        }
+    bool exist(const ISBN& inde){
+        return list.exist(inde);
     }
-
-    void showall() {
-        Block new_;
-        int i = 0;
-        while (i != -1) {
-            getin(i, new_);
-            if(i==0&&new_.size==0){
+    void find(const ISBN &inde){
+        list.find(inde);
+    }
+    void showall(){
+        block__<ISBN,Book>tmp;
+        int i=0;
+        while(i!=-1){
+            list.myread(i,tmp);
+            if(i==0&&tmp.size==0){
                 cout<<'\n';
                 return;
             }
-            for(int k=0;k<new_.size;k++){
-                cout<<new_.ele[k]<<'\n';
+            for(int k=0;k<tmp.size;k++){
+                cout<<tmp.ele[k].valu<<'\n';
             }
-            i=new_.next;
+            i=tmp.next;
         }
-    };
-    Block getblock(const ISBN inde) {
-        Block new_;
-        int i = 0;
-        //bool fin = 0;
-        while (i != -1) {
-            getin(i, new_);
-            if (new_.size == 0) {
-                i = new_.next;
-                continue;
-            }
-            if ((new_.mini.isbn <= inde) && (inde <= new_.maxi.isbn)) {
-                myread(i, new_);
-                return new_;
-            } else if (inde < new_.mini.isbn) {
-                break;
-            }
-            i = new_.next;
-        }
-        error("Invalid");
     }
-    bool exist(ISBN &inde){
-        Block new_;
-        int i = 0;
-        //bool fin = 0;
-        while (i != -1) {
-            getin(i, new_);
-            if (new_.size == 0) {
-                i = new_.next;
-                continue;
-            }
-            if ((new_.mini.isbn <= inde) && (inde <= new_.maxi.isbn)) {
-                myread(i, new_);
-                for (int k = 0; k < new_.size; ++k) {
-                    if (new_.ele[k].isbn == inde) {
-                        return true;
-                    }
-                }
-            } else if (inde < new_.mini.isbn) {
-                break;
-            }
-            i = new_.next;
-        }
-        return false;
-    }
-
     void show_name(const Name& name_){
-        Block new_;
+        block__<ISBN,Book> new_;
         bool get=0;
         int i=0;
         while(i!=-1){
-            getin(i,new_);
+            list.myread(i,new_);
             if (new_.size == 0) {
                 i = new_.next;
                 continue;
             }
-            myread(i, new_);
+            //list.myread(i, new_);
             for (int k = 0; k < new_.size; ++k) {
-                if(new_.ele[k].name==name_){
+                if(new_.ele[k].valu.name==name_){
                     get=1;
-                    cout<<new_.ele[k]<<'\n';
+                    cout<<new_.ele[k].valu<<'\n';
                 }
             }
             i=new_.next;
@@ -489,20 +244,20 @@ public:
         if(!get)cout<<'\n';
     };
     void show_author(Author& author_){
-        Block new_;
+        block__<ISBN,Book> new_;
         bool get=0;
         int i=0;
         while(i!=-1){
-            getin(i,new_);
+            list.myread(i,new_);
             if (new_.size == 0) {
                 i = new_.next;
                 continue;
             }
-            myread(i, new_);
+            //list.myread(i, new_);
             for (int k = 0; k < new_.size; ++k) {
-                if(new_.ele[k].author==author_){
+                if(new_.ele[k].valu.author==author_){
                     get=1;
-                    cout<<new_.ele[k]<<'\n';
+                    cout<<new_.ele[k].valu<<'\n';
                 }
             }
             i=new_.next;
@@ -510,40 +265,51 @@ public:
         if(!get)cout<<'\n';
     };
     void show_keyword(Keyword & keyword_){
-        Block new_;
+        block__<ISBN,Book> new_;
         bool get=0;
         int i=0,j=0;
         vector<string> words;
         string word;
         while(i!=-1){
-            getin(i,new_);
+            list.myread(i,new_);
             if (new_.size == 0) {
                 i = new_.next;
                 continue;
             }
-            myread(i, new_);
+            //myread(i, new_);
             for (int k = 0; k < new_.size; ++k) {
-                while(new_.ele[k].keyword.keyword_[j]!='\0'){
-                    if(new_.ele[k].keyword.keyword_[j]=='|'||new_.ele[k].keyword.keyword_[j+1]=='\0'){
+                //cout<<new_.ele[k].valu.keyword<<' ';
+                while(new_.ele[k].valu.keyword.keyword_[j]!='\0'){
+                    if(new_.ele[k].valu.keyword.keyword_[j]=='|'||new_.ele[k].valu.keyword.keyword_[j+1]=='\0'){
+                        if(new_.ele[k].valu.keyword.keyword_[j+1]=='\0'){
+                            word+=new_.ele[k].valu.keyword.keyword_[j];
+                        }
                         words.push_back(word);
+                        word="";
                         j++;
                         continue;
                     }
-                    word[j]+=new_.ele[k].keyword.keyword_[j];
+                    word+=new_.ele[k].valu.keyword.keyword_[j];
                     j++;
                 }
+                //cout<<words[0]<<' '<<word[1];
                 for(auto iter=words.begin();iter!=words.end();iter++){
+                    //cout<<*iter<<' ';
+
                     Keyword kk(*iter);
                     if(keyword_==kk){
+                        //cout<<111;
                         get=1;
-                        cout<<new_.ele[k]<<'\n';
+                        cout<<new_.ele[k].valu<<'\n';
                     }
                 }
+                words.clear();
+                j=0;
             }
             i=new_.next;
         }
         if(!get)cout<<'\n';
     }
-
 };
+
 #endif //CODE_BOOK_HPP
